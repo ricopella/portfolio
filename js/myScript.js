6d645b194be63785b525f7c3c206557e123ce518
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-27 12:39:20 -0700

    added fade-in effect for intro text

--- js/myScript.js
+++ js/myScript.js
@@ -1,28 +1,33 @@
+// Intro-Text Fade-In
+$(() => {
+    $('.intro').textillate();
+})
+
 //Code to get a smooth scroll from menu items to section
 
 // slide effects - top
-$("#nav-heading").on("click", function() {
+$("#nav-heading").on("click", () => {
     $('html, body').animate({
         scrollTop: $("#heading").offset().top
     }, 1000);
 });
 
 // slide effects - portfolio
-$("#nav-portfolio").on("click", function() {
+$("#nav-portfolio").on("click", () => {
     $('html, body').animate({
         scrollTop: $("#portfolio").offset().top
     }, 1000);
 });
 
 // slide effects
-$("#nav-about-me").on("click", function() {
+$("#nav-about-me").on("click", () => {
     $('html, body').animate({
         scrollTop: $("#about-me").offset().top
     }, 1000);
 });
 
 // slide effects
-$("#nav-contact").on("click", function() {
+$("#nav-contact").on("click", () => {
     $('html, body').animate({
         scrollTop: $("#contact").offset().top
     }, 1000);
